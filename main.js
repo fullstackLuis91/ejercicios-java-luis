13aa2573f9cacfb93f2b3f4ff98a551827b63b38
fullstackLuis91 <luis_rv91@hotmail.com>
2024-10-08 09:25:41 +0200

    ejercicios terminados

---
+++ main.js
@@ -0,0 +1,151 @@
+//  alert('JavaScript funcionando correctamente')
+let nombre = "pepe"
+const boolean1 = true
+const boolean2 = false
+const pi = 3.14;
+const tau = 3.14 * 2
+const miNombre = "luis"
+const boolean = true
+console.log(miNombre.length)
+console.log(typeof miNumeroFav)
+console.log('my name is ${luis} and i am ${33}')
+const texto=  "Seré un crack en JavaScript al terminar el bootcamp"
+console.log(texto.toUpperCase()) 
+const texto2= "hola soy un crack"
+console.log(texto2.substring(0,4))
+miNumeroFav = "9"
+console.log(typeof miNumeroFav)
+const texto3= "PI"
+console.log(texto3.substring(0-2))
+let arrayVacio = []
+let arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+let arrayNumeros2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+let arrayNumerosPares = [0, 2, 4, 6, 8]
+let arrayNumerosNeg = [0, -1, -2, -3, -4, -5, -6, -7, -8, -9]
+let holaMundo = ["hola", "Mundo"]
+let loGuardoTodo = ["hola", "que", 23, 42.33, "tal" ]
+let arrayDeArrays = [[756, 'nombre'], [225, 'apellido'], [298, 'direccion']]
+console.log(loGuardoTodo.length)
+loGuardoTodo.unshift ("moto")
+console.log(loGuardoTodo)
+console.log(loGuardoTodo [2])
+const coche = {
+    marca: "bmw",
+    modelo: "m3",
+    matricula: "2341tgf"
+}
+const casa = {
+    codpostal: 46024,
+    calle: "avenida-del-puerto",
+    portal: 112,
+    piso: 6
+}
+const FullStackDeveloper = {
+    lenguajes: "javascript"-"css"-"html",
+    proyectos: "mi pagina personal" 
+}
+const perro = {
+    nombre: "tomi",
+    edad: 8,
+    color: "blanco y marrón",
+    raza: "perro de agua",   
+}
+const noticia = {
+    titular: "luis se convierte en desarrollador web",
+    cuerpo: "Tras mucho esfuerzo, luis lo ha conseguido"
+}
+const persona = {
+    nombre: "Luis",
+    apellidos: "Rioja Valdés",
+    edad: "33"
+}
+console.log(persona)
+console.log(FullStackDeveloper.lenguajes[0])
+const portatil = {
+    marca: "lenovo"
+}
+console.log(portatil.marca)
+const Concierto = {
+    grupos: ["los delincuentes", "estopa"]
+}
+console.log(Concierto.grupos);
+const led = {
+    lámpara1: "rojo",
+    lámpara2: "verde",
+    lámpara3: "azul"    
+}
+const rgb= ["led.lámpara1, led.lámpara2, led.lámpara3"]
+console.log(rgb)
+const O_Error= {
+    código: 9
+}
+const códigoerror= O_Error.código
+console.log(códigoerror)
+const grupo= {
+    integrantes: ["luis,dani,juan"] 
+}
+const integrantes= grupo.integrantes
+console.log(integrantes)
+const impresora= {
+    tinta: {
+        rojo:100,
+        verde:90,
+        azul:80 
+    }
+}
+const nivelesTinta= impresora.tinta
+console.log(nivelesTinta)
+const Movil= {
+    especificaciones: {
+        marca: "xiaomi",
+        procesador: "snapdragon"
+    }
+}
+const especificaciones= Movil.especificaciones
+console.log(especificaciones)
+const Portatil= {
+    marca: "lenovo"
+}  
+Portatil.marca= "msi"  
+console.log(Portatil)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
